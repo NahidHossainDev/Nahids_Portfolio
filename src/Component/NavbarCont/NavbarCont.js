45183f3410fa001974037991a1dd48c011747447
nahid9764 <nahidhossaain9764@gmail.com>
2020-11-25 19:05:26 +0600

    add particel js

--- src/Component/NavbarCont/NavbarCont.js
+++ src/Component/NavbarCont/NavbarCont.js
@@ -45,41 +45,11 @@ const NavbarCont = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto navbar">
-            <Nav.Link
-              href="/home"
-              className="navItem"
-              style={{ color: "white" }}
-            >
-              Home
-            </Nav.Link>
-            <Nav.Link
-              href="/portfolio"
-              className="navItem"
-              style={{ color: "white" }}
-            >
-              Portfolio
-            </Nav.Link>
-            <Nav.Link
-              href="/blog"
-              className="navItem"
-              style={{ color: "white" }}
-            >
-              Blog
-            </Nav.Link>
-            <Nav.Link
-              href="/aboutMe"
-              className="navItem"
-              style={{ color: "white" }}
-            >
-              About me
-            </Nav.Link>
-            <Nav.Link
-              href="/contact"
-              className="navItem"
-              style={{ color: "white" }}
-            >
-              Contact
-            </Nav.Link>
+            <Nav.Link href="/home" className="navItem" style={{ color: "white" }} > Home </Nav.Link>
+            <Nav.Link href="/portfolio" className="navItem" style={{ color: "white" }} >Portfolio </Nav.Link>
+            <Nav.Link href="/blog" className="navItem" style={{ color: "white" }} >  Blog </Nav.Link>
+            <Nav.Link href="/aboutMe" className="navItem" style={{ color: "white" }} >  About me  </Nav.Link>
+            <Nav.Link href="/contact" className="navItem" style={{ color: "white" }} > Contact </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </div>
